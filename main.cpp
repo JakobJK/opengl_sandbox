42eb1c41b6d21b4d82428596902379dc12ed5fda
Jakob Kousholt <jakobjk@gmail.com>
2022-12-29 15:31:57 -0500

    wip

--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
 #include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
 int main()
 {
 	glfwInit();
-	GLFWwindow* w = glfwCreateWindow(600, 600, "Polies & Ponies!", NULL, NULL);
-	glfwMakeContextCurrent(w);
-	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	float t[] =
-	{
-		-.5f, -.5f, 0.f,
-		0.f, .5f, 0.f,
-		.5f, -.5f, 0.f
-	};
-	uint32_t v;
-	glGenBuffers(1, &v);
-	glBindBuffer(GL_ARRAY_BUFFER, v);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(t)*sizeof(t)/sizeof(t[0]),
-		&t[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float)*3,
-		 (void*)0);
-	glEnableVertexAttribArray(0);
-	glViewport(0, 0, 600, 600);
-	while(!glfwWindowShouldClose(w))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glfwSwapBuffers(w);
-		glfwPollEvents();
-	}
-	glfwTerminate();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, FLFW_OPENGL_CORE_PROFILE);
 
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFrameBufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((Gladloadproc)glfwGetProcAddresds))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	while(!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
